66163b3ad616b62876f100605e378eb6952abcf8
Arnoud <damylen@gmail.com>
2015-11-18 21:00:31 +0100

    updated editWithCode to start typescript watch + nodemon

--- editWithCode.bat
+++ editWithCode.bat
@@ -1,4 +1,9 @@
 start /B code .
 start /B code ../csWeb/csComp
 start /B code ../csWeb/csServerComp
+start tsc -p ../csWeb/csServerComp -w
+start tsc -p ../csWeb/csComp -w
+start tsc -w
+start nodemon server
+cd ../csWeb/
 gulp
