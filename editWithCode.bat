0a53366e2b697ac345cd7a3159662951ed7ba77a
Arnoud <damylen@gmail.com>
2015-11-19 16:01:36 +0100

    editWithCode starts csWeb instead of different projects

--- editWithCode.bat
+++ editWithCode.bat
@@ -1,9 +1,8 @@
 start /B code .
-start /B code ../csWeb/csComp
-start /B code ../csWeb/csServerComp
+start /B code ../csWeb
 start tsc -p ../csWeb/csServerComp -w
-start tsc -p ../csWeb/csComp -w
-start tsc -w
+start tsc -p ../csWeb/csComp -w 
+start tsc -w 
 start nodemon server
 cd ../csWeb/
 gulp
