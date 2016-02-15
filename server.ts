86bdafa2c9f86fe927634b300dad21a97500c38b
Rinze Bruining <rbruining@alten.nl>
2016-02-15 15:06:12 +0100

    Added example layers showing all buildings in the Netherlands from the a BAG-database (when zoomed in to ~neighborhood-level). Set the startDatabaseConnection to 'true' to be able to use it. For best performance it is recommended to use a local BAG-database (configurable through configuration.json).

--- server.ts
+++ server.ts
@@ -6,7 +6,9 @@ Winston.add(Winston.transports.Console, <Winston.ConsoleTransportOptions>{
     colorize: true,
     label: 'csWeb',
     prettyPrint: true
-})
+});
+
+var startDatabaseConnection = false;
 
 var cs = new csweb.csServer(__dirname, <csweb.csServerOptions>{
     port: 3003,
@@ -14,6 +16,17 @@ var cs = new csweb.csServer(__dirname, <csweb.csServerOptions>{
     //connectors: { mqtt: { server: 'localhost', port: 1883 }, mongo: { server : '127.0.0.1', port: 27017} }
 });
 cs.start(() => {
+
+    if (startDatabaseConnection) {
+        this.config = new csweb.ConfigurationService('./configuration.json');
+        this.config.add('server', 'http://localhost:' + cs.options.port);
+        var bagDatabase = new csweb.BagDatabase(this.config);
+        var mapLayerFactory = new csweb.MapLayerFactory(<any>bagDatabase, cs.messageBus, cs.api);
+        cs.server.post('/bagcontours', (req, res) => {
+            mapLayerFactory.processBagContours(req, res);
+        });
+    }
+
     console.log('really started');
     //    //{ key: "imb", s: new ImbAPI.ImbAPI("app-usdebug01.tsn.tno.nl", 4000),options: {} }
     //    var ml = new MobileLayer.MobileLayer(api, "mobilelayer", "/api/resources/SGBO", server, messageBus, cm);
